2f9001c4cc7bb07314405c456767af400db65aee
Robinson <robinson0993@gmail.com>
2022-01-22 13:10:51 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+//ejecutar scripts desde el boton del html mediante onclick
+const ejercicio = () => {
+
+    // validar que el numero este en el rango entre 1 y 20
+    const validar = (num) => {
+        if (num >= 1 && num <= 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // funcion para multiplicar el numero indicado
+    const multiplicar = (a) => {
+        for (let i = 1; i <= a; i++) {
+            console.log(`${i} x ${num} =`, i * a);
+        }
+    }
+
+    // funcion para obtener el factorial del numero indicado
+    const factorial = (a) => {
+        var res = 1;
+        for (let i = 1; i <= a; i++) {
+            res *= i;
+            console.log(`factorial de ${i} es : ${res}`);
+        }
+    }
+
+    // Solicitar el numero y almacenar en variable para obtner los resultados mediante las funciones
+    var num = prompt("Ingresae Numero entre el rango (1 - 20)", 5);
+
+    // Verificar si numero es mayo igual a 1 y menor igual que 20
+    if (validar(num) == false) {
+        alert("Numero fuera del rango, intenta nuevamente con otro num ( 1 - 20)");
+
+    } else {
+        multiplicar(num);
+        factorial(num);
+    }
+}
